31815731ceb3a664976d6bf0a5027dec6c30e449
asya-code <77514066+asya-code@users.noreply.github.com>
2022-11-18 15:17:13 -0800

    files added

---
+++ BestMovieService.java
@@ -0,0 +1,17 @@
+package com.javaunit3.springmvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BestMovieService {
+    @Autowired
+    private Movie movie;
+    public Movie getBestMovie() {
+        return movie;
+    }
+    @Autowired
+    public BestMovieService(Movie movie){
+        this.movie = movie;
+    }
+
+}
---
+++ Movie.java
@@ -0,0 +1,10 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public interface Movie {
+    public String getTitle();
+    public String getMaturityRating();
+    public String getGenre();
+}
