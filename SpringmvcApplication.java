9f3bbc3d94a8d0046e68d3c4cdace02241c8e474
asya-code <77514066+asya-code@users.noreply.github.com>
2022-11-18 20:33:50 -0800

    Springmvc and Movie controller added

---
+++ SpringmvcApplication.java
@@ -0,0 +1,10 @@
+package com.javaunit3.springmvc;
+
+import org.springframework.boot.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcApplication {
+    public static void main(String[] args) {
+        SpringBootApplication.run(SpringBootApplication.class, args);
+    }
+}
